2cf60c34f9a4c95962bd04e8da9b6dbcf15ce61b
haitrieu123580 <haitrieu123580@gmail.com>
2023-08-03 15:33:52 +0700

    test api

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import LoginForm from './components/LoginForm/LoginForm.vue';
+</script>
+
+<template>
+  <header>
+
+  </header>
+  <main>
+    <notifications />
+    <LoginForm/>
+  </main>
+    
+</template>
---
+++ vue-project/src/components/testAxios.vue
@@ -0,0 +1,18 @@
+<template>
+    <div></div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default {
+    mounted() {
+        axios.post('http://localhost:3000/auth/login', {
+        username: 'trieu',
+        password: 'password'
+      })
+      .then((response) => console.log(response))
+
+    }
+  }
+  </script>
